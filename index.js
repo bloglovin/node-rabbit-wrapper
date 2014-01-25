da73fb0f9302ec543d286fddacc5d015bd8141ca
Patrik Ring <me@patrikring.se>
2014-01-24 21:32:19 -0500

    reconnect on lost rabbit connection

--- index.js
+++ index.js
@@ -9,10 +9,64 @@
 //
 
 var amqp = require('amqplib');
+var EventEmitter = require('events').EventEmitter;
 
 var Rabbit = function (config) {
-  this.connection = amqp.connect(config.host);
-}
+  this.config           = config;
+  this.config.heartbeat = config.heartbeat || 1;
+  this.initial_timeout  = 1000;
+  this.max_retries      = 30;
+  this.connection       = null;
+  this.eventer          = new EventEmitter();
+
+  // Initial connection
+  this.connect(1);
+};
+
+Rabbit.prototype.connect = function (tries) {
+  var self = this;
+
+  self.connection = null;
+
+  if (tries > this.max_retries) {
+    var err = new Error('Rabbit: Too many connection attempts');
+    throw err;
+  }
+
+  tries++;
+
+  // Returns a promise of a connection
+  self.connection = amqp.connect(self.config.host);
+
+  self.connection.then(function (conn) {
+    // Connected
+    // Tell your friends
+    self.eventer.emit('connected');
+    // Reset our retry counter
+    tries = 1;
+
+    // Bind for new errors
+    conn.once('error', function (e) {
+      self.eventer.emit('error', e);
+    });
+
+    // If disconnect, we want to try to connect again
+    self.eventer.on('error', function (e) {
+      setTimeout(function () {
+        self.connect.call(self, tries);
+      }, self.initial_timeout);
+    });
+  }).then(null, function (e) {
+    self.eventer.emit('connect_failed', e);
+    // We end up here if the first promise
+    // fails us. That is, there's no rabbit to connect to
+    setTimeout(function () {
+      console.log('called');
+      self.connect.call(self, tries);
+    }, (self.initial_timeout * (tries - 1)));
+  });
+
+};
 
 //
 // ## run rabbit
@@ -27,7 +81,7 @@ var Rabbit = function (config) {
 //
 // * **onConnect** callback(err, channel) runs this when we have a connection to rabbit
 //
-Rabbit.prototype.go = function (onConnect, errorHandler) {
+Rabbit.prototype.go = function (onConnect) {
   var self = this;
 
   // Use the promise of a connection
@@ -39,9 +93,16 @@ Rabbit.prototype.go = function (onConnect, errorHandler) {
     channel.then(function (ch) {
       onConnect(null, ch);
     });
+  });
 
+  self.eventer.on('error', function (e) {
+    self.eventer.on('connected', function () {
+      self.go(onConnect);
+    });
   });
 };
+
+// Rabbit.prototype.disconnect = function ()
 
 module.exports = function (config) {
   return new Rabbit(config);
--- test/index.test.js
+++ test/index.test.js
@@ -4,8 +4,49 @@ var rabbit = require('./../index');
 var config = {host: "amqp://localhost"};
 var con = new rabbit(config);
 var expect = require('chai').expect;
+var sinon = require('sinon');
 
 describe('Rabbit wrapper', function () {
+  describe('Connection', function () {
+    it('should emit event connected', function (done) {
+      con.eventer.once('connected', function (e) {
+        done();
+      });
+    });
+
+    it('should reconnect on error', function (done) {
+      var clock = sinon.useFakeTimers();
+
+      con.eventer.once('error', function (e) {
+        con.eventer.once('connected', function () {
+          done();
+        });
+      });
+
+      con.eventer.emit('error');
+      clock.tick(1100);
+      clock.restore();
+    });
+
+    it('should only try to reconnect x times on error', function () {
+      var rabbiter = new rabbit(config);
+      var fn = function () {
+        rabbiter.connect((rabbiter.max_retries + 1));
+      };
+
+      expect(fn).to.throw(/Too many/);
+    });
+
+    it('should fail first time', function (done) {
+      var rabbiter = new rabbit({host: "amqp://nohost"});
+      rabbiter.max_retries = 1;
+
+      rabbiter.eventer.on('connect_failed', function (e) {
+        done();
+      });
+    });
+  });
+
   describe('go', function () {
     it('Should have a channel', function (done) {
       con.go(function (err, channel) {
@@ -13,6 +54,22 @@ describe('Rabbit wrapper', function () {
         expect(channel).to.be.a('object');
         done();
       });
+    });
+
+    it('should set up new channel on error', function (done) {
+      var rabbiter = new rabbit(config);
+      var spy = sinon.spy();
+
+      rabbiter.go(function (ch) {
+        spy(ch);
+        var clock = sinon.useFakeTimers();
+        clock.tick(1100);
+        expect(spy.calledTwice).to.be.ok;
+        clock.restore();
+        done();
+      });
+
+      rabbiter.eventer.emit('error', 'Error');
     });
 
     it('Should be able to send, receive and ack a message', function (done) {
